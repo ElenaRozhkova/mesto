e0aee361dedf53faecc013979c201febfab086a0
Elena Rozhkova <elenakuzmina88@yandex.ru>
2021-04-17 08:38:34 +0300

    fix: исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,22 @@
-const profileEdit = document.querySelector('.profile__editbutton');
-const profileClose = document.querySelector('.popup__close-icon');
+let profileEdit = document.querySelector('.profile__editbutton');
+let profileClose = document.querySelector('.popup__close-icon');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-const popupContainer = document.querySelector('.popup__container');
-const popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
+let nameInput = document.querySelector('.popup_type_name');
+let jobInput = document.querySelector('.popup_type_job');
 
-for (let button of document.querySelectorAll(".element__vector")) {
-    button.addEventListener("click", function() {
-        this.classList.toggle('element__vector_active');
-    });
+
+function openfunc(){   
+    popup.classList.add('popup_opened');
+    nameInput.textContent=profileName.value;
+    jobInput.textContent=profileJob.value;
 }
 
-popupContainer.insertAdjacentHTML('beforeend',
-    `<h2 class="popup__edit">Редактировать профиль</h2>
-    <input type="text" class="popup__name" id="name" name="nameInput" placeholder="${profileName.textContent}" required>
-    <input type="text" class="popup__job" id="job" name="jobInput" placeholder="${profileJob.textContent}" required>
-    <input type="submit" value="Сохранить" class="popup__button">
-    `);
-
-const formElement = document.querySelector('.popup__button');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-
-function clearValueName() {
-    nameInput.placeholder = '';
+function closefunc(){
+    popup.classList.remove('popup_opened');
 }
 
-function clearValueJob() {
-    jobInput.placeholder = '';
-}
-
-function tooglePopup() {
-    popup.classList.toggle('popup_opened');
-    profileName = document.querySelector('.profile__name');
-    profileJob = document.querySelector('.profile__job');
-    nameInput.textContent = profileName.textContent;
-    jobInput.textContent = profileJob.textContent;
-}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -44,11 +24,9 @@ function formSubmitHandler(evt) {
         profileName.textContent = nameInput.value;
         profileJob.textContent = jobInput.value;
     }
-    tooglePopup();
+    closefunc();
 }
 
-profileEdit.addEventListener('click', tooglePopup);
-profileClose.addEventListener('click', tooglePopup);
-nameInput.addEventListener('click', clearValueName);
-jobInput.addEventListener('click', clearValueJob);
-formElement.addEventListener('click', formSubmitHandler);
+profileEdit.addEventListener('click',openfunc);
+profileClose.addEventListener('click',closefunc);
+popup.addEventListener('submit', formSubmitHandler);
