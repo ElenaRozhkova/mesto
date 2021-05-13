6900518d0d167e6221ce7715030b27c616ed4b18
unknown <elenakuzmina88@yandex.ru>
2021-05-13 20:06:23 +0200

    fix:add config variable

--- scripts/validate.js
+++ scripts/validate.js
@@ -73,12 +73,4 @@ const removeInputError = (formElement, inputList, validation) => {
     });
 };
 
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible',
-    formSet: '.popup__form-set'
-});
+enableValidation(elemObject);
